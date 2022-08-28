418edfc63d0f597f76c69983382e286705c01b7e
otarik <otarr20@gmail.com>
2022-08-28 13:20:15 +0300

    Completed logic for three buttons in XCraftAction

--- app/src/main/java/com/demoexamp/android/loginacttivity/XCraftAction.java
+++ app/src/main/java/com/demoexamp/android/loginacttivity/XCraftAction.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class XCraftAction extends AppCompatActivity {
-
+    private int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,36 +22,57 @@ public class XCraftAction extends AppCompatActivity {
         setContentView(R.layout.activity_x_craft_action);
     }
 
-    public void openFile(View view) {
 
-        TextView infotv =findViewById(R.id.info_tv);
-        StringBuilder stringBuilder= new StringBuilder();
+    public void openFile(View view) {
+        String fileName = "";
+
+        switch (view.getId()) {
+            case R.id.button_comet:
+                fileName = "comet.txt";
+                break;
+            case R.id.button_planet:
+                fileName = "planet.txt";
+                break;
+            case R.id.button_list:
+                count++;
+                if (count % 2 == 0) {
+                    findViewById(R.id.button_comet).setVisibility(View.VISIBLE);
+                    findViewById(R.id.button_planet).setVisibility(View.VISIBLE);
+                } else {
+                    findViewById(R.id.button_comet).setVisibility(View.GONE);
+                    findViewById(R.id.button_planet).setVisibility(View.GONE);
+                }
+                break;
+        }
+
+
+        TextView infotv = findViewById(R.id.info_tv);
+        StringBuilder stringBuilder = new StringBuilder();
 
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(
-                    new InputStreamReader(getApplicationContext().getAssets().open("comet.txt"), "UTF-8"));
-   //          do reading, usually loop until end of file reading
+                    new InputStreamReader(getApplicationContext().getAssets().open(fileName), "UTF-8"));
+            //          do reading, usually loop until end of file reading
             String mLine;
             while ((mLine = reader.readLine()) != null) {
- //              process line
+                //              process line
                 stringBuilder.append(mLine);
             }
-                infotv.setText(stringBuilder.toString());
+            infotv.setText(stringBuilder.toString());
 
         } catch (IOException e) {
-  //          log the exception
+            //          log the exception
         } finally {
             if (reader != null) {
                 try {
                     reader.close();
                 } catch (IOException e) {
-       //             log the exception
+                    //             log the exception
                 }
             }
 
         }
 
     }
-
 }
